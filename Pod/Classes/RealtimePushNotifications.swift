66ec5b182cf27d29fadc25416cf29470aa5e946a
João Parreira <jparreira@realtime.co>
2016-09-19 18:11:28 +0100

    Version 1.0.0

---
+++ Pod/Classes/RealtimePushNotifications.swift
@@ -0,0 +1,122 @@
+//
+//  RealtimePushNotifications.swift
+//  OrtcClient
+//
+//  Created by joao caixinha on 21/01/16.
+//  Copyright © 2016 Realtime. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/**
+ * OrtcClientPushNotificationsDelegate process custom push notification with payload
+ */
+public protocol OrtcClientPushNotificationsDelegate{
+
+    /**
+     * Process custom push notifications with payload.
+     * If receive custom push and not declared in AppDelegate class trow's excpetion
+     * - parameter channel: from witch channel the notification was send.
+     * - parameter message: the remote notification title
+     * - parameter payload: a dictionary containig the payload data
+     */
+    func onPushNotificationWithPayload(_ channel:String, message:String, payload:NSDictionary?)
+}
+
+/**
+ * UIResponder extenssion for auto configure application to use remote notifications
+ */
+extension UIResponder: OrtcClientPushNotificationsDelegate{
+    
+/**
+     Overrides UIResponder initialize method
+*/
+    override open class func initialize() {
+        NotificationCenter.default.addObserver(self.self, selector: #selector(UIResponder.registForNotifications), name: NSNotification.Name.UIApplicationDidFinishLaunching, object: nil)
+    }
+    
+    @available(iOS, deprecated: 1.0, message: "For iOS older versions")
+    static func registForNotifications() -> Bool {
+        if UIApplication.shared.responds(to: #selector(UIApplication.registerUserNotificationSettings(_:))) {
+            let settings: UIUserNotificationSettings = UIUserNotificationSettings(types:[.alert, .badge, .sound], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+        else {
+            UIApplication.shared.registerForRemoteNotifications(matching: [.sound, .alert, .badge])
+        }
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        var newToken: String = (deviceToken as NSData).description
+        newToken = newToken.trimmingCharacters(in: CharacterSet(charactersIn: "<>"))
+        newToken = newToken.replacingOccurrences(of: " ", with: "")
+        print("\n\n - didRegisterForRemoteNotificationsWithDeviceToken:\n\((deviceToken as NSData).description)\n")
+        OrtcClient.setDEVICE_TOKEN(newToken)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void){
+        completionHandler(UIBackgroundFetchResult.newData)
+        self.application(application, didReceiveRemoteNotification: userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        if (userInfo["C"] as? NSString) != nil && (userInfo["M"] as? NSString) != nil && (userInfo["A"] as? NSString) != nil {
+            var t:NSDictionary? = (userInfo["aps"] as? NSDictionary)
+            if (((userInfo["aps"] as? NSDictionary)?["alert"]) is String) {
+                let ortcMessage: String = "a[\"{\\\"ch\\\":\\\"\(userInfo["C"] as! String)\\\",\\\"m\\\":\\\"\(userInfo["M"] as! String)\\\"}\"]"
+                
+                var notificationsDict: NSMutableDictionary?
+                if UserDefaults.standard.object(forKey: NOTIFICATIONS_KEY) != nil{
+                     notificationsDict = NSMutableDictionary(dictionary: UserDefaults.standard.object(forKey: NOTIFICATIONS_KEY) as! NSDictionary)
+                }
+                if notificationsDict == nil {
+                    notificationsDict = NSMutableDictionary()
+                }
+                
+                var notificationsArray: NSMutableArray?
+                
+                if notificationsDict?.object(forKey: userInfo["A"] as! String) != nil{
+                    notificationsArray = NSMutableArray(array: notificationsDict?.object(forKey: userInfo["A"] as! String) as! NSArray)
+                }
+                
+                if notificationsArray == nil{
+                    notificationsArray = NSMutableArray()
+                }
+                
+                notificationsArray!.add(ortcMessage)
+                notificationsDict!.setObject(notificationsArray!, forKey: (userInfo["A"] as! String as NSCopying))
+                UserDefaults.standard.set(notificationsDict!, forKey: NOTIFICATIONS_KEY)
+                UserDefaults.standard.synchronize()
+                NotificationCenter.default.post(name: Notification.Name(rawValue: "ApnsNotification"), object: nil, userInfo: userInfo)
+            }
+            else if((UIApplication.shared.delegate?.responds(to: "onPushNotifications:message:payload:")) != nil){
+                (UIApplication.shared.delegate as! UIResponder).onPushNotificationWithPayload(userInfo["C"] as! String,
+                    message: userInfo["M"] as! String,
+                    payload: userInfo["aps"] as? NSDictionary)
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        NSLog("Failed to register with error : %@", error)
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "ApnsRegisterError"), object: nil, userInfo: [
+            "ApnsRegisterError" : error
+            ]
+        )
+    }
+    
+    /**
+     * Process custom push notifications with payload.
+     * If receive custom push and not declared in AppDelegate class trow's excpetion
+     * - parameter channel: from witch channel the notification was send.
+     * - parameter message: the remote notification title
+     * - parameter payload: a dictionary containig the payload data
+     */
+    open func onPushNotificationWithPayload(_ channel:String, message:String, payload:NSDictionary?){
+            preconditionFailure("Must override onPushNotificationWithPayload method on AppDelegate")
+    }
+
+}
---
+++ RealtimeMessaging-iOS-Swift3-Push.podspec
@@ -0,0 +1,31 @@
+#
+# Be sure to run `pod lib lint RealtimeMessaging-iOS-Swift3.podspec' to ensure this is a
+# valid spec before submitting.
+#
+# Any lines starting with a # are optional, but their use is encouraged
+# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
+#
+
+Pod::Spec.new do |s|
+  s.name             = "RealtimeMessaging-iOS-Swift3-Push"
+  s.version          = "1.0.0"
+  s.summary          = "Swift3 Realtime Cloud Messaging (ORTC) SDK for iOS with push notifications catgory"
+
+  s.description      = <<-DESC
+      Part of the The Realtime® Framework, Realtime Cloud Messaging (aka ORTC) is a secure, fast and highly scalable cloud-hosted Pub/Sub real-time message broker for web and mobile apps.
+
+      If your website or mobile app has data that needs to be updated in the user’s interface as it changes (e.g. real-time stock quotes or ever changing social news feed) Realtime Cloud Messaging is the reliable, easy, unbelievably fast, “works everywhere” solution.
+  DESC
+
+  s.homepage         = "https://github.com/realtime-framework/RealtimeMessaging-iOS-Swift3-Push"
+  s.license          = 'MIT'
+  s.author           = { "Realtime.co" => "framework@realtime.co" }
+  s.source           = { :git => "https://github.com/realtime-framework/RealtimeMessaging-iOS-Swift3-Push.git", :tag => s.version}
+  s.social_media_url = 'https://twitter.com/RTWworld'
+
+  s.platform     = :ios, '10.0'
+  s.requires_arc = true
+  s.source_files = 'Pod/Classes/**/*'
+  s.dependency 'RealtimeMessaging-iOS-Swift3', '1.0.0'
+  s.dependency 'Starscream', '1.1.5'
+end
